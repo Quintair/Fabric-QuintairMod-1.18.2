7bd6f496519d21b938e9e59832f59d2ca0b4df81
Quintair <Raitins050@gmail.com>
2022-02-19 14:35:09 +1000

    Init

--- src/main/java/ru/quintair/quintairmod/item/ModItems.java
+++ src/main/java/ru/quintair/quintairmod/item/ModItems.java
@@ -9,10 +9,10 @@ import ru.quintair.quintairmod.QuintairMod;
 public class ModItems {
     //TODO Сделать крафты в 1-ну ячейку.
 
-    public static final Item JADE_INGOT = registerItem("jade_ingot",
+    public static final Item JADE_INGOT = ModItems.registerItem ("jade_ingot",
             new Item(new FabricItemSettings().group(ModItemGroup.QUINTAIRGROUP).maxCount(64)));
 
-    public static final Item JADE_NUGGET = registerItem("jade_nugget",
+    public static final Item JADE_NUGGET = ModItems.registerItem("jade_nugget",
             new Item(new FabricItemSettings().group(ModItemGroup.QUINTAIRGROUP).maxCount(64)));
 
 
@@ -21,7 +21,6 @@ public class ModItems {
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(QuintairMod.MOD_ID, name), item);
     }
-
     public static void registerModItems(){
         QuintairMod.LOGGER.info("Registering Mod Items for " + QuintairMod.MOD_ID);
     }
